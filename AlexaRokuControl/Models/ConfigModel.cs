bcb36e7af39a36470a9e185252259a7d8b3158a6
Michael Stephens <harros@gmail.com>
2018-02-26 14:38:40 -0800

    Initial Commit

---
+++ AlexaRokuControl/Models/ConfigModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlexaRokuControl.Models.Config
+{
+    public class Vizio
+    {
+        public string TVUrl { get; set; }
+        public string SoundBarUrl { get; set; }
+        public string VizioAuth { get; set; }
+    }
+
+    public class APISettings
+    {
+        public string RokuUrl { get; set; }
+        public Vizio Vizio { get; set; }
+    }
+}
